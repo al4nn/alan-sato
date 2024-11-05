40da272c830bac34b901e3f0f11f718d673808c5
Alan Akira Sato Alves <58231117+al4nn@users.noreply.github.com>
2024-11-05 15:26:32 -0300

    feat: printando a primeira mensagem na tela

---
+++ Curso Desenvolvimento Web Completo/PHP Básico/Printar mensagens na tela/index.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+    <title>Minha Página de teste</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <p>
+        <?php
+
+        echo "Olá Mundo!";
+
+        ?>
+    </p>
+</body>
+
+</html>
