574f61fa64df87910f842861411ba92db6a907f5
Alan Sato <alan.sato96@outlook.com>
2025-01-07 15:45:10 -0300

    :fire: remove arquivos da branch

--- Curso Desenvolvimento Web Completo/5 - Projeto 01/painel/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $(".painel header .menu").on("click", function () {
-        $(".painel header").toggleClass("active");
-        $(".painel .wrapper aside").toggleClass("active");
-        $(".painel .wrapper aside .profile").toggleClass("active");
-        $(".painel .wrapper main").toggleClass("active");
-    });
-});
