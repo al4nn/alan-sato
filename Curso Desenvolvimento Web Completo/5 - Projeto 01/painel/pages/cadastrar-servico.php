975cf71aa0b1ac58e1584d0eb80ea0400f4dd56a
Alan Sato <alan.sato96@outlook.com>
2025-01-07 16:54:08 -0300

    :blue_book: cria pagina para cadastro dos servicos usando o sistema de cadastro dinamico

---
+++ Curso Desenvolvimento Web Completo/5 - Projeto 01/painel/pages/cadastrar-servico.php
@@ -0,0 +1,37 @@
+<?php checkPermissionPage(1); ?>
+
+<div class="box-painel cadastrar-depoimento">
+    <div class="title flex align-center">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+            <g id="SVGRepo_iconCarrier">
+                <path d="M11 12H3"></path>
+                <path d="M16 6H3"></path>
+                <path d="M16 18H3"></path>
+                <path d="M18 9v6" stroke="#0f0"></path>
+                <path d="M21 12h-6" stroke="#0f0"></path>
+            </g>
+        </svg>
+        <h2>
+            Cadastrar Serviço
+        </h2>
+    </div>
+
+    <form method="post" enctype="multipart/form-data">
+        <input type="hidden" name="name_table" value="tb_site.servicos" />
+        <?php if (isset($_POST['acao'])) {
+            if (Painel::insert($_POST)) {
+                Painel::alert('success', 'Serviço cadastrado com sucesso!', '');
+            } else {
+                Painel::alert('error', 'Campos vázios não são permitidos!', 'Tente novamente!');
+            }
+        } ?>
+        <div class="form-group">
+            <textarea name="service" rows="4" placeholder="Serviço"></textarea>
+        </div>
+        <div class="form-group">
+            <input class="fw-700" type="submit" name="acao" value="Cadastrar!" />
+        </div>
+    </form>
+</div>
